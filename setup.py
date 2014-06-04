e6a61f376562eb3c0266e7c847ad6139a2d48167
Matt O'Donnell <matt@absolutod.com>
2014-06-04 09:21:26 -0400

    * fixed spacing * removed old way I was doing deps * added termcolor as a dep

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
+# setup.py
+# encoding: utf-8
+
 from setuptools import setup, find_packages
 
-deps = ['setuptools']
-try:
-    import argparse
-except ImportError:
-    deps.append('argparse')
-    
 import newman
 
 setup(
-  name='newman',
-  version=newman.get_version(),
-  description='Simple utility for making command tools.  Inspired by rake.',
-  author="Matt O'Donnell",
-  author_email='matt.odonnell@adkeeper.com',
-  packages=['newman'],
-  install_requires=deps,
-  entry_points={
-    'console_scripts':[
-      'newman = newman.core:main',
+    name='newman',
+    version=newman.get_version(),
+    description='Simple utility for making command tools.  Inspired by rake.',
+    author="Matt O'Donnell",
+    author_email='matt@absolutod.com',
+    packages=find_packages(),
+    install_requires=[
+        'docutils',
+        'termcolor'
+    ],
+    entry_points={
+        'console_scripts':[
+            'newman = newman.core:main',
+            ]
+    },
+    classifiers=[
+        'Operating System :: POSIX',
+        'Environment :: Console'
     ]
-  },
-  classifiers=[
-    'Operating System :: POSIX',
-    'Environment :: Console'
-  ]
 )
