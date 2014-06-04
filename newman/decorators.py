c3f0e1b5e177b60f683980dbf033dbff4aaa9f78
Matt O'Donnell <matt@absolutod.com>
2014-06-04 10:31:09 -0400

    * fixed spacing

--- newman/decorators.py
+++ newman/decorators.py
@@ -3,16 +3,17 @@
   Decorators
 """
 
-def task(options):
-  def define_task(func):
-    def new_func(*args, **kwargs):
-      return func(*args, **kwargs)
+
+def task(options=()):
+    def define_task(func):
+        def new_func(*args, **kwargs):
+            return func(*args, **kwargs)
     
-    new_func.func_name = func.func_name
-    new_func.func_doc = func.func_doc
-    new_func.__dict__ = func.__dict__
-    new_func.task = True
-    new_func.options = options
-    return new_func
+        new_func.func_name = func.func_name
+        new_func.func_doc = func.func_doc
+        new_func.__dict__ = func.__dict__
+        new_func.task = True
+        new_func.options = options
+        return new_func
   
-  return define_task
+    return define_task
